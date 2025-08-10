22ae0884b4753cd76f03c98e9343ff3d873c65cf
hcharon <hcharon@softia.fr>
2025-08-10 18:28:59 +0200

    test coverage a supprimer

--- src/app/services/jokes.service.spec.ts
+++ src/app/services/jokes.service.spec.ts
@@ -1,21 +1,72 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { JokesService } from './jokes.service';
-import {HttpClientTestingModule} from "@angular/common/http/testing";
+import { of } from 'rxjs';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {Joke} from "../model/joke.model";
+import {AppComponent} from "../app.component";
+import {JokesService} from "./jokes.service";
 
-describe('JokesService', () => {
-  let service: JokesService;
 
-  beforeEach(() => {
-      TestBed.configureTestingModule({
-        imports: [HttpClientTestingModule],
-        providers: [JokesService]
-      });
-      service = TestBed.get(JokesService);
+// import { TestBed } from '@angular/core/testing';
+//
+// import { JokesService } from './jokes.service';
+// import {HttpClientTestingModule} from "@angular/common/http/testing";
+//
+// describe('JokesService', () => {
+//   let service: JokesService;
+//
+//   beforeEach(() => {
+//       TestBed.configureTestingModule({
+//         imports: [HttpClientTestingModule],
+//         providers: [JokesService]
+//       });
+//       service = TestBed.get(JokesService);
+//   });
+//
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
+//
+// });
+
+
+describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let jokeServiceSpy: jasmine.SpyObj<JokesService>;
+
+  const mockJoke: Joke = {
+    joke: 'Pourquoi les devs n’aiment pas la lumière ?',
+    response: 'Parce qu’ils préfèrent les bugs !'
+  };
+
+  beforeEach(async () => {
+    const spy = jasmine.createSpyObj('JokesService', ['joke$', 'getRandomJoke']);
+
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      providers: [{ provide: JokesService, useValue: spy }],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA] // ignore Angular Material tags
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+
+    jokeServiceSpy = TestBed.inject(JokesService) as jasmine.SpyObj<JokesService>;
+    jokeServiceSpy.joke$.and.returnValue(of(mockJoke));
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
   });
 
+  it('should get a joke on init', () => {
+    component.ngOnInit();
+    expect(jokeServiceSpy.getRandomJoke).toHaveBeenCalled();
+  });
+
+  it('should call getRandomJoke when button clicked', () => {
+    component.getRandomJoke();
+    expect(jokeServiceSpy.getRandomJoke).toHaveBeenCalled();
+  });
 });
